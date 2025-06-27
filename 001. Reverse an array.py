3ce21b124a23e269411188fb5c04148017c78019
violet-autumn <164505123+violet-autumn@users.noreply.github.com>
2025-06-27 15:27:29 +0530

    Update 001. Reverse an array.py

--- 001. Reverse an array.py
+++ 001. Reverse an array.py
@@ -2,10 +2,15 @@
 
 def reverseArray(a):
     size = len(a)
-    # print(size)
-    # print(a[size-1])
-    # print(a[0])
     b = []
     for i in range(0, size):
         b.append(a[size-i-1])
+    return b
+
+
+def reverseArray(a):
+    size = len(a)
+    b = [0]*size
+    for i in range(0, size):
+        b[size-i-1]=a[i]
     return b
