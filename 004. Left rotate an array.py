140c6a082d2b58940a8cdec91e520a9125b74743
violet-autumn <164505123+violet-autumn@users.noreply.github.com>
2025-06-27 15:52:39 +0530

    Update 004. Left rotate an array.py

--- 004. Left rotate an array.py
+++ 004. Left rotate an array.py
@@ -7,3 +7,27 @@ def rotateLeft(d, arr):
         new_pos = (i-d)%size
         result[new_pos]=arr[i]
     return result
+
+#-----------------------------------------#
+
+# The following appraoch reduces calculations
+
+def rotateLeft(d, arr):
+    size = len(arr)
+    new_arr = [0]*size
+    
+    new_pos = (0-d)%size
+    i = 0 
+    breaker = new_pos
+    
+    #fill from breaker index to end of new array
+    for new_pos in range (breaker,size):    
+        new_arr[new_pos] = arr[i] 
+        i = i+1   
+
+    #fill for 0 to breaker index of new array
+    for new_pos in range (0, breaker):       
+        new_arr[new_pos] = arr[i]
+        i=i+1
+    
+    return new_arr
