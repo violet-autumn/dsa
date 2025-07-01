f5f2737dde4009e2a492dd96982599dc51524a8d
violet-autumn <164505123+violet-autumn@users.noreply.github.com>
2025-07-01 16:03:23 +0530

    Update 009. Insert node at head.py

--- 009. Insert node at head.py
+++ 009. Insert node at head.py
@@ -1,3 +1,5 @@
+# www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list?isFullScreen=true
+
 def insertNodeAtHead(head, data):
     new_node = SinglyLinkedListNode(data)
     new_node.next = head
