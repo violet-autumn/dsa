a4526ae824d23ab2f3e534011c403f4375563d70
violet-autumn <164505123+violet-autumn@users.noreply.github.com>
2025-07-01 11:42:37 +0530

    Update 000. Operations.py

--- 000. Operations.py
+++ 000. Operations.py
@@ -1,14 +1,28 @@
 # Array
 
-a = []                     														# Declares an empty appar
-a.append(<elemet>) 																		# Appends element in the array
-size = len(a)																					# len(a) returns the length of the array 
-b = [0] * size																				# Creates an array b of the length "size" and initializes all elements to 0
-print(a)																							# Prints the array
+# Declares an empty array
+a = []                   
 
-arr = [[0 for _ in range(cols)] for _ in range(rows)]	# Creates a 2D array of size rows x cols and initializes all elements with 0. Eg: [[0,0], [0,0], [0,0]]
-arr = [[] for _ in range(rows)]												# Creates an empty nested 2D array. Eg: [[], [], []]
-row_size = len(arr)																		# Calculating row and col lengths for 2D array
+# Appends element in the array
+a.append(<elemet>) 		
+
+# len(a) returns the length of the array 
+size = len(a)				
+
+# Creates an array b of the length "size" and initializes all elements to 0
+b = [0] * size				
+
+# Prints the array
+print(a)									 
+
+# Creates a 2D array of size rows x cols and initializes all elements with 0. Eg: [[0,0], [0,0], [0,0]]
+arr = [[0 for _ in range(cols)] for _ in range(rows)]	
+
+# Creates an empty nested 2D array. Eg: [[], [], []]
+arr = [[] for _ in range(rows)]			
+
+# Calculating row and col lengths for 2D array
+row_size = len(arr)																		
 col_size = len(arr[0])
 
 
