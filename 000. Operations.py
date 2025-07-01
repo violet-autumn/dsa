2eddf6fdaab12e7e9db43e104c9dd9c09c7e7376
violet-autumn <164505123+violet-autumn@users.noreply.github.com>
2025-07-01 11:45:46 +0530

    Update 000. Operations.py

--- 000. Operations.py
+++ 000. Operations.py
@@ -1,27 +1,28 @@
 # Array
 
-# Declares an empty array
+# Declare an empty array
 a = []                   
 
-# Appends element in the array
+# Append element in the array
 a.append(<elemet>) 		
 
 # len(a) returns the length of the array 
 size = len(a)				
 
-# Creates an array b of the length "size" and initializes all elements to 0
+# Create an array b of the length "size" and initialize all elements 0
 b = [0] * size				
 
-# Prints the array
+# Print the array and then print the element at the 5th index
 print(a)									 
+print(a[i])
 
-# Creates a 2D array of size rows x cols and initializes all elements with 0. Eg: [[0,0], [0,0], [0,0]]
+# Create a 2D array of size rows x cols and initialize all elements with 0. Eg: [[0,0], [0,0], [0,0]]
 arr = [[0 for _ in range(cols)] for _ in range(rows)]	
 
-# Creates an empty nested 2D array. Eg: [[], [], []]
+# Create an empty nested 2D array. Eg: [[], [], []]
 arr = [[] for _ in range(rows)]			
 
-# Calculating row and col lengths for 2D array
+# Calculate row and col lengths for 2D array
 row_size = len(arr)																		
 col_size = len(arr[0])
 
