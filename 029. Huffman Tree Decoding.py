3c2854981307cf2e79535a6732557206b6a26e6a
violet-autumn <164505123+violet-autumn@users.noreply.github.com>
2025-07-05 01:44:53 +0530

    Update 029. Huffman Tree Decoding.py

--- 029. Huffman Tree Decoding.py
+++ 029. Huffman Tree Decoding.py
@@ -22,3 +22,36 @@ def decodeHuff(root, s):
     
     # Print the result
     print(answer)
+
+
+#------------------------------------------------------------------------------------------------------------#
+
+# The following approach traverses the entire tree once and builds a decoder dict, thereby saving the comute to go through the tree for every letter
+
+def assign(root, decoder, string):
+    # If the node is a leaf node, assign the character in decoder dict
+    if root.left is None: 
+        decoder[string] = root.data
+    else:
+        assign(root.left, decoder, string + '0')
+        assign(root.right, decoder, string + '1')
+
+def decodeHuff(root, s):
+    
+    # Define a dictionary that will store the decoded characters
+    decoder = {}
+    
+    # Decode the characters from the tree
+    assign(root, decoder, '')
+    
+    # Parse the input string and return the answer
+    answer = ""
+    temp = ""
+    it = 0
+    for it in range (0, len(s)):
+        temp += s[it]
+        if temp in decoder:
+            answer += decoder[temp]
+            temp = ""
+    
+    print(answer)
