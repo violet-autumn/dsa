d3512ace8772efab56a57d65a251f989e34bb61f
violet-autumn <164505123+violet-autumn@users.noreply.github.com>
2026-03-02 13:25:47 +0530

    Update 03. longest substring without repeating characters.py

--- blind-75/03. longest substring without repeating characters.py
+++ blind-75/03. longest substring without repeating characters.py
@@ -3,12 +3,11 @@ class Solution:
         sub = ""
         length = 0
         for i in s:
-            parts = sub.split(i, 1)
-            sub = parts[1] if len(parts) > 1 else sub
+            if i in sub:
+                parts = sub.split(i, 1)
+                sub = parts[1] if len(parts) > 1 else sub
             sub = sub + i
-            print(sub)
             if len(sub) > length:
                 length = len(sub)
 
         return length
-        
